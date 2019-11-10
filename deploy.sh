4d8aa952e2c65546f0215719fae29d2d2dbddba5
Jordan Laman <jordan.laman@cigna.com>
2019-11-10 13:26:43 -0800

    added deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t jordandlaman/multi-client:latest -t jordandlaman/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jordandlaman/multi-server:latest -t jordandlaman/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jordandlaman/multi-worker:latest -t jordandlaman/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push jordandlaman/multi-client:latest
+docker push jordandlaman/multi-server:latest
+docker push jordandlaman/multi-worker:latest
+
+docker push jordandlaman/multi-client:$SHA
+docker push jordandlaman/multi-server:$SHA
+docker push jordandlaman/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=jordandlaman/multi-server:$SHA
+kuebctl set image deployments/client-deployment client=jordandlaman/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jordandlaman/multi-worker:$SHA
